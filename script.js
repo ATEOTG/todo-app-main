e3aac4a8c0ed059104e66a36aa62fb49519029a9
ATEOTG <bryansantos9797@gmail.com>
2023-01-07 19:02:57 -0500

    added filter functionality to todo list

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const icon = document.querySelector(".icon");
 const form = document.querySelector(".search-form");
 const textVal = document.querySelector(".input-search");
 const todoList = document.querySelector(".todo-list");
+const itemsActive = document.querySelector(".item-length-text");
+const clearCompleted = document.querySelector(".clear-text");
+
+const options = document.querySelectorAll(".option");
 
 icon.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
@@ -13,8 +17,9 @@ icon.addEventListener("click", function () {
 });
 
 form.addEventListener("submit", addTodo);
-
 todoList.addEventListener("click", itemHandler);
+clearCompleted.addEventListener("click", clearItems);
+options.forEach((el) => el.addEventListener("click", optionSelected));
 
 function addTodo(e) {
   e.preventDefault();
@@ -32,6 +37,7 @@ function addTodo(e) {
 </li>`;
 
   todoList.insertAdjacentHTML("beforeend", html);
+  increaseItem();
   textVal.value = "";
 }
 
@@ -40,9 +46,66 @@ function itemHandler(e) {
   if (target.matches(".circle")) {
     const todoItem = target.closest(".todo-item");
     todoItem.classList.toggle("completed");
+    todoItem.classList.contains("completed") ? decreaseItem() : increaseItem();
   }
   if (target.matches(".cross")) {
     const todoItem = target.closest(".todo-item");
     todoItem.remove();
+    if (todoItem.classList.contains("completed")) return;
+    decreaseItem();
+  }
+}
+
+function increaseItem() {
+  const itemLengthArr = itemsActive.innerHTML.split("");
+  itemLengthArr[0] = +itemLengthArr[0] + 1;
+  itemsActive.innerHTML = itemLengthArr.join("");
+}
+function decreaseItem() {
+  const itemLengthArr = itemsActive.innerHTML.split("");
+  itemLengthArr[0] = +itemLengthArr[0] - 1;
+  itemsActive.innerHTML = itemLengthArr.join("");
+}
+
+function clearItems() {
+  const todoItems = document.querySelectorAll(".todo-item");
+  for (const item of todoItems) {
+    if (item.classList.contains("completed")) item.remove();
+  }
+}
+
+function optionSelected(e) {
+  const id = e.target.id;
+  for (const option of options) {
+    if (option.id != id) {
+      option.classList.remove("selected");
+      continue;
+    }
+    option.classList.add("selected");
+  }
+
+  filterItems(id);
+}
+
+function filterItems(id) {
+  const todoItems = document.querySelectorAll(".todo-item");
+  if (id === "all") {
+    for (const item of todoItems) {
+      item.style.display = "flex";
+    }
+  } else if (id === "active") {
+    for (const item of todoItems) {
+      if (item.classList.contains("completed")) item.style.display = "none";
+      else {
+        item.style.display = "flex";
+      }
+    }
+  } else {
+    for (const item of todoItems) {
+      if (!item.classList.contains("completed")) item.style.display = "none";
+      else {
+        item.style.display = "flex";
+      }
+    }
   }
 }
